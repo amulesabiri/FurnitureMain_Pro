5e41affa279c7f0a98372ab70a15ef21ff85b47b
amulesabiri <118688739+amulesabiri@users.noreply.github.com>
2022-11-20 22:38:00 +0300

    Add files via upload

---
+++ information.js
@@ -0,0 +1,332 @@
+//*
+// const constant = 
+// */
+
+const constant = {
+    "nav_log": "url",
+
+    "nav_note": [
+        "Furniture That Everyone Loves",
+        "Confort Seat suit with brands",
+    ],
+
+    "nav_routes" : {
+        "home" : {
+            "title": "Home",
+            "location": "/"
+        },
+        "aboutus" : {
+            "title": "About us",
+            "location": "/aboutus"
+        },
+        "furniture" : {
+            "title": "Furniture",
+            "location": "/furniture"
+        },
+        "contactus" : {
+            "title": "Contact Us",
+            "location": "/contactus"
+        },
+        "login" : {
+            "title": "Login",
+            "location":"/login"
+        },
+        "signup" : {
+            "title": "Signup",
+            "location": "/signup"
+        },
+        "cart" : {
+            "title": "Cart",
+            "icon": "fa fa-shopping-cart",
+            "location": "/cart"
+        }
+    },
+
+    
+    "socials" : [
+        {"title": "twitter","link": "https://www.twitter.com/","icon": "fa fa-twitter"},
+        {"title": "facebook","link": "https://www.facebook.com/","icon": "fa fa-facebook"},
+        {"title": "instagram","link": "https://www.instagram/","icon": "fa fa-instagram"},
+        {"title": "linkedin","link": "https://www.linkedin.com/","icon": "fa fa-linkedin"}
+    ],
+
+    "address" : "45 Grand Central Terminal New York.",
+    
+    "phone" : "+256708093144",
+
+    "open_hours" : {
+        "open" : "09:00",
+        "close" : "18:00"
+    },
+
+    "categories" :[
+        {
+        "general_name" : "louge chairs",
+        "category_name" : "baby sites",
+        "category thumbnail" : "https://url",
+        "product":[
+            {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        }]
+        },
+
+
+        
+        
+        
+        
+        
+        
+        {
+        "general_name" : "louge chairs",
+        "category_name" : "baby sites",
+        "category thumbnail" : "https://url",
+        "product":[
+            {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        }]
+        },
+
+
+
+        {
+        "general_name" : "louge chairs",
+        "category_name" : "baby sites",
+        "category thumbnail" : "https://url",
+        "product":[
+            {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        },
+        {
+            "name" : "supreme seat",
+            "product_thumbnail" : "url",
+            "color" : "black",
+            "order" : "new",
+            "manufacturer": "supreme",
+            "rating" : 5,
+            "real_price" : 500.99,
+            "discount_price" : 400.99,
+            "discount" : 10, // 10% off
+            "material" : "leather",
+            "designer" : "jambgimo",
+            "made" : "UK",
+            "size" : {
+                "width" : "5ft",
+                "height" : "2ft"
+            },
+            "warranty" : 350 // 350 days
+        }]
+        }
+    ]/* end of category */
+
+
+
+
+
+}
+
+
+
+
+
+module.exports = constant;
